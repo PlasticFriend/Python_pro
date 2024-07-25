3de7473c2b1064002971ea0fc64c0734539c0285
PlasticFriend <sayan02.official@gmail.com>
2024-07-26 02:10:08 +0530

    learnt fstring and doc strings

---
+++ doc_stings.py
@@ -0,0 +1,13 @@
+def square(n):
+    '''
+
+    :param n: takes in a number
+    :return: the square of the number
+    '''
+    res = n*n
+    return res
+print(square(3))
+print(square.__doc__)
+# doc string should be right below the def statement
+# PEP 8-----Python Enhancement Proposal
+# Check in the terminal "Import This"------must
---
+++ f_strings_.py
@@ -0,0 +1,7 @@
+letter = "My name is {} and i am from {}"
+country = "India"
+name = "sayan"
+print(letter.format(name, country))
+#now using ftrings
+print(f"Hey my name is{name} and I am from {country}")
+
