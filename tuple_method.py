db853f670bfef21e702e915e8260a44bfa713278
PlasticFriend <sayan02.official@gmail.com>
2024-07-25 17:20:27 +0530

    learnt tuples and tuple methods

---
+++ tuple_method.py
@@ -0,0 +1,9 @@
+# tuple can be converted to a list andmodified and then changed again to tuple
+# can add tuples
+tup1 = (1,2,3,4,5)
+tup2 = (7,8,9,10)
+tup = tup1+tup2
+print(tup1.count(3))
+print(tup1.index(3))
+print(tup1.index(3,1,4)) # will find index of 3 between the index 1 and 4
+# can also use len method
---
+++ tuples_.py
@@ -0,0 +1,7 @@
+tup = (1,2,4,6) #if you want a tuple of single element then comma is necessery
+print(tup)
+print(type(tup))
+print(tup[3])
+if 2 in tup:
+    print("yes")
+# slicing possible in tuple but a new tuple is generated
