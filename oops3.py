59605fb726878cfdc285507cc4a3c729868279e4
PlasticFriend <sayan02.official@gmail.com>
2024-07-28 20:58:14 +0530

    oops part 1 completed

--- oops3.py
+++ oops3.py
@@ -3,16 +3,19 @@ Attributes
 two types of attributes
 1. Class attributes----same for all the objects in the class
 2. object/instance attributes-----defined by self.<attribute>
-3.
+3.object attribute>class attribute
 
 
 '''
 
 class Person:
     college = "ABC college"
+    name = 'anonymous'
+
     def __init__(self,n,o): # --- parameterised constructer
         self.name = n
         self.occ = o
+        self.college
     def __init__(self):  # ---Defalut constructer
         print("hey i am a person")
 
